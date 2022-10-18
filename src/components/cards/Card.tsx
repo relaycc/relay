e868abf011aa7c0f8b36599d23c0c95d7d7ab423
brian schafer <roycepdev@gmail.com>
2022-10-18 13:57:09 -0700

    Removed overflow hidden

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -9,7 +9,7 @@ export const Card = ({
 }) => {
   return (
     <div
-      className={`w-[18rem] h-[24rem] flex flex-col shadow-lg hover:shadow-xl rounded-md gap-4 p-4 border-[3px] border-black bg-white overflow-hidden ${className}`}
+      className={`w-[18rem] h-[24rem] flex flex-col shadow-lg hover:shadow-xl rounded-md gap-4 p-4 border-[3px] border-black bg-white ${className}`}
     >
       {children}
     </div>
