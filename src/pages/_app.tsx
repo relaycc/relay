497e0aa4c50bd191e1e4cee907e1c69780a7f680
Brian S <81312527+BrianS111@users.noreply.github.com>
2022-10-21 10:02:42 -0700

    Use custom page titles
    * `/ `-> title is "Relay"
    * `/u/:handle` -> title is "handle"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import { WagmiConfig, configureChains, createClient, chain } from "wagmi";
 import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import Head from "next/head";
 // TODO(achilles@relay.cc) For some reason rainbowkit css import wasn't working,
 // remove this hack soon.
 import "../styles/rainbowkit.css";
@@ -38,13 +39,18 @@ const wagmiClient = createClient({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <Receiver>
-          <Component {...pageProps} />
-        </Receiver>
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <>
+      <Head>
+        <title>Relay</title>
+      </Head>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <Receiver>
+            <Component {...pageProps} />
+          </Receiver>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </>
   );
 }
 
