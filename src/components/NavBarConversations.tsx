58fae4825511136e6a841c7c6dd9b7e464073b23
achilles schmelzer <achilles@daopanel.com>
2023-01-14 14:28:39 -0800

    First draft

--- src/components/NavBarConversations.tsx
+++ src/components/NavBarConversations.tsx
@@ -4,19 +4,18 @@ import {
   isLensName,
   useLaunch,
 } from "@relaycc/receiver";
-import { useState } from "react";
-import { useRouter } from "next/router";
+import { FunctionComponent, useState } from "react";
 import { IconSwap } from "./icons/IconSwap";
 import { IconWallet } from "./icons/IconWallet";
-import { IconNetwork } from "./icons/IconNetwork";
-import {
-  useAccountModal,
-  useChainModal,
-  useConnectModal,
-} from "@rainbow-me/rainbowkit";
-import { useAccount, useNetwork } from "wagmi";
+import { useAccountModal, useConnectModal } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
 import { IconSearch } from "./icons/IconSearch";
 import Image from "next/image";
+import { Discord } from "./icons/Discord";
+import { Twitter } from "./icons/Twitter";
+import { Github } from "./icons/Github";
+import { Mirror } from "./icons/Mirror";
+import Link from "next/link";
 
 interface NavbarConversationsProps {
   handleAddressSearch: (e: string) => unknown;
@@ -25,29 +24,22 @@ interface NavbarConversationsProps {
 export const NavBarConversations = ({
   handleAddressSearch,
 }: NavbarConversationsProps) => {
-  const [input, setInput] = useState<string | null>(null);
-  const [inputIsError, setInputIsError] = useState(false);
   const connectModal = useConnectModal();
   const accountModal = useAccountModal();
-  const chainModal = useChainModal();
-  const network = useNetwork();
   const account = useAccount();
+  const [input, setInput] = useState<string | null>(null);
+  const [inputIsError, setInputIsError] = useState(false);
   const launch = useLaunch();
-  const router = useRouter();
 
   return (
-    <nav className="xl:flex items-end grid grid-cols-2 gap-4 mb-6 mt-5">
-      <div className="sm:w-[164px] min-h-[47px] w-[125px] xl:min-w-[174px] relative top-[4px] md:top-[3px]">
-        <Image
-          src={'/relaylogo.svg'}
-          alt={"Relay"}
-          layout="fill"
-        />
-      </div>
+    <div className="fixed left-0 right-0 bg-white">
+      <nav className="flex items-center mt-0 bg-white p-6 rounded-lg">
+        <div className="sm:w-[164px] min-h-[47px] w-[125px] xl:min-w-[174px] relative top-[4px] md:top-[3px]">
+          <Image src={"/relaylogo.svg"} alt={"Relay"} layout="fill" />
+        </div>
 
-      <div className="w-full flex justify-end order-3 col-span-2">
         <form
-          className="relative xl:w-3/4 w-full"
+          className="relative ml-12 mr-12 w-full"
           onSubmit={(e) => {
             e.preventDefault();
             if (isLensName(input) || isEnsName(input) || isEthAddress(input)) {
@@ -62,7 +54,7 @@ export const NavBarConversations = ({
           <input
             type="text"
             placeholder="Enter ENS, Lens, or ETH Address"
-            className={`focus:outline-none border-[3px] border-black bg-gray-100 h-12 p-5 rounded-lg w-full pr-12 ${
+            className={`hover:bg-gray-200 focus:bg-gray-200 focus:outline-none border-[3px] border-black bg-gray-100 h-12 p-5 rounded-lg w-full pr-12 ${
               inputIsError && "focus:border-2 focus:border-red-200"
             }`}
             onChange={(e) => {
@@ -76,29 +68,43 @@ export const NavBarConversations = ({
             <IconSearch />
           </div>
         </form>
-      </div>
+        <ul className="ml-auto flex items-center gap-4 mr-4 min-w-32">
+          <a
+            className="text-xl hover:underline"
+            href={"https://twitter.com/relay_eth"}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            className="text-xl hover:underline"
+            href={"https://discord.gg/DTMKf63ZSf"}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Discord
+          </a>
+          <a
+            className="text-xl hover:underline"
+            href={"https://mirror.xyz/relaycc.eth"}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Blog
+          </a>
+          <a href="https://github.com/relaycc" target="_blank" rel="noreferrer">
+            <Github />
+          </a>
+        </ul>
 
-      <div className="flex justify-end items-center gap-4 ml-auto xl:order-3 order-2 md:w-auto xs:w-full">
-        <button
-          onClick={chainModal.openChainModal}
-          className={`hidden sm:flex btn btn-ghost bg-accent w-40 border-none rounded-md`}
-        >
-          <div className="flex flex-row items-center flex-grow justify-between">
-            {network.chain?.name || "No Network"}
-          </div>
-          {account.isConnected ? (
-            <IconSwap onClick={() => null} />
-          ) : (
-            <IconNetwork />
-          )}
-        </button>
         <button
           onClick={
             account.isConnected
               ? accountModal.openAccountModal
               : connectModal.openConnectModal
           }
-          className={`btn btn-ghost bg-accent lg:w-[319px] border-none rounded-md`}
+          className={`btn btn-ghost bg-accent lg:w-[240px] border-none rounded-md ml-0 hover:bg-orange-200`}
         >
           <div className="flex flex-row flex-grow items-center justify-between">
             {account.isConnected && typeof account.address === "string"
@@ -111,7 +117,7 @@ export const NavBarConversations = ({
             )}
           </div>
         </button>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
