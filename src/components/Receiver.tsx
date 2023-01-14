336c2ff4914ab79719ad1c1629f0eac9d1e21a36
achilles schmelzer <achilles@daopanel.com>
2023-01-14 07:45:43 -0800

    Add styled-components

---
+++ src/components/Receiver.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Receiver = styled.div`
+  height: 700px;
+  width: 360px;
+  border: 4px solid black;
+  border-radius: 4px;
+  margin: 2rem auto;
+`;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { GlobalStyles } from "@/components/GlobalStyles";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
