6228cb5736325dfc0100d85e4d58adba5b020399
brian schafer <roycepdev@gmail.com>
2023-01-15 19:14:02 -0800

    Fixed typo that was blocking build

--- src/pages/defi.tsx
+++ src/pages/defi.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-export default function defi () {
+export default function Defi () {
     const router = useRouter()
     useEffect(() => {
         router.push('/?category=defi', undefined, { shallow: true })
