0c6dc0e346f960355459b17f9e0352d8cc5be7bc
Csillag Katalin <122450390+CsillagK@users.noreply.github.com>
2023-01-17 16:45:00 +0100

    Closes #166

---
+++ src/lib/design/MsgBox.tsx
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import {ArrowUpCircle} from "@/lib/design/ArrowUpCircle";
+import {receiverTheme} from "@/lib/design/wip/receiverTheme";
+import {textSmallRegular} from "@/lib/design/wip/typography";
+
+const Root = styled.div`
+  display: flex;
+  
+  width: 100%;
+  //width: 360px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  
+  padding: 0.625rem 1rem;
+  border-radius: 1.5rem;
+  background-color: ${receiverTheme.colors.gray["100"]};
+  width: 100%;
+`;
+
+const Input = styled.input`
+  ${textSmallRegular};
+  background-color: ${receiverTheme.colors.gray["100"]};
+  color: ${receiverTheme.colors.gray["900"]};
+  border: hidden;
+  
+  :focus{
+    outline: none;
+  }
+  
+  ::placeholder{
+    ${textSmallRegular};
+    color: ${receiverTheme.colors.gray["400"]};
+  }
+  width: 94%;
+`;
+
+
+export const MsgBox = ({active}: {active: boolean}) => {
+
+        if (active) {
+            return (
+                <Root>
+                    <Wrapper>
+                        <Input placeholder={"Type a Message"} />
+                        <ArrowUpCircle active={active} />
+                    </Wrapper>
+                </Root>
+            )}
+        return (
+            <Root>
+                <Wrapper>
+                    <Input placeholder={"Type a Message"} />
+                    <ArrowUpCircle active={active} />
+                </Wrapper>
+            </Root>
+        )
+
+
+
+}
