10092d2947e890f667d34bcb462e57fe069e905a
achilles schmelzer <achilles@daopanel.com>
2023-01-16 23:31:48 -0800

    Add spinners

---
+++ src/lib/design/LoaderAnimGeneral.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+export const LoaderAnimGeneral = () => {
+  return (
+    <FixedSizeDiv
+      animate={{ rotate: 360 }}
+      transition={{
+        repeat: Infinity,
+        duration: 1,
+        repeatDelay: 0,
+        ease: "linear",
+      }}
+    >
+      <Spinner src="/loader-black.png" alt="loader" width={24} height={24} />
+    </FixedSizeDiv>
+  );
+};
+
+const FixedSizeDiv = styled(motion.div)`
+  height: 24px;
+  width: 24px;
+  padding: 0;
+`;
+
+const Spinner = styled(Image)`
+  height: 24px;
+  width: 24px;
+`;
