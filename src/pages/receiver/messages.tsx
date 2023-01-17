d065d56c60d8221d8f9f660924fa66ea0d5af75f
brian schafer <roycepdev@gmail.com>
2023-01-16 20:01:08 -0800

    Removed testing environment

--- src/pages/receiver/messages.tsx
+++ src/pages/receiver/messages.tsx
@@ -1,11 +1,3 @@
-import { Copy } from "@/lib/design/Copy";
-
-const handleClick = () => {
-  console.log('hello')
-}
-
 export default function Messages() {
-  return <main style={{marginTop: '100px', marginLeft: '100px'}}>
-    <Copy hoverText="Copy Address" onCopy={handleClick} />
-  </main>;
+  return <main>This is the user messages page!</main>;
 }
