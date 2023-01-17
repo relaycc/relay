12d61745a9a25c19975ef4bd1194fcd1a3538f7a
csillagk <katalin.csillag@emergence-engineering.com>
2023-01-17 11:18:18 +0100

    added animation

--- src/lib/design/CloseIcon.tsx
+++ src/lib/design/CloseIcon.tsx
@@ -6,21 +6,23 @@ const Svg = styled.svg`
   
   :hover{
     rect{
+      transition: fill 150ms ease-in;
       fill: #F2F4F7;
     }
   }
   
   :active{
     rect{
+      transition: fill 150ms ease-in;
       fill: #D5D0DD;
     }
   }
 `;
 
-export const Close = () => (
+export const CloseIcon = () => (
     <Svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect width="40" height="40" rx="20" fill="white"/>
-        <path d="M26 14L14 26M14 14L26 26" stroke="#101828" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M26 14L14 26M14 14L26 26" stroke="#101828" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </Svg>
 
 )
