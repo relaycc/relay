a9b2e67c45d69f7c37f72966e6ffebb51879f89e
Norbert-EE <norbert.aschenbrenner@memrise.com>
2023-01-17 13:33:53 +0100

    Created base component layout with some props, added Xmtp icon

---
+++ src/lib/design/InitializeXmtp.tsx
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+import { XmptIcon } from "@/lib/design/XmptIcon";
+import { textMdSemiBold, textSmallRegular } from "@/lib/design/wip/typography";
+
+const ComponentRoot = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 1rem;
+  gap: 1rem;
+  border-radius: ${(props) => props.theme.radius.m};
+  background-color: ${(props) => props.theme.colors.primary["100"]};
+  box-shadow: 0 1px 3px rgba(16, 24, 40, 0.1), 0 1px 2px rgba(16, 24, 40, 0.06);
+`
+
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  gap: 1rem;
+`
+
+const RowItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  
+  button {
+    width: 100%;
+    height: 60px;
+  }
+`
+
+const Title = styled.div`
+    ${textMdSemiBold};
+    color: ${(props) => props.theme.colors.primary["700"]};
+`
+
+const Subtitle = styled.div`
+    ${textSmallRegular};
+    color: ${(props) => props.theme.colors.gray["600"]};
+`
+
+interface InitializeXmtpProps {
+  isLoading: boolean;
+  handleSignin: () => void;
+}
+
+export const InitializeXmtp = ({isLoading, handleSignin}: InitializeXmtpProps) => {
+  return (
+    <ComponentRoot>
+      <Row>
+        <XmptIcon/>
+        <RowItem>
+          <Title>Initialize XMTP</Title>
+          <Subtitle>Please connect with XMTP to start messaging.</Subtitle>
+        </RowItem>
+      </Row>
+      <ButtonWrapper>
+        {isLoading ? (
+          <button>Signing in</button>
+        ) : (
+          <button onClick={handleSignin}>Sign-in with XMTP</button>
+          )}
+      </ButtonWrapper>
+    </ComponentRoot>
+  )
+}
