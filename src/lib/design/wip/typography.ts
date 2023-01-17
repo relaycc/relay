aa7caca85c96cf2fa1f994ee4b1355d49c9e3b31
achilles schmelzer <achilles@daopanel.com>
2023-01-17 00:27:41 -0800

    Add requests and some typography

--- src/lib/design/wip/typography.ts
+++ src/lib/design/wip/typography.ts
@@ -13,3 +13,18 @@ export const textSmallRegular = css`
   vertical-align: middle;
   font-weight: 400;
 `;
+
+export const textXsMedium = css`
+  font-size: 12px;
+  line-height: 18px;
+  font-weight: 500;
+  font-style: normal;
+`;
+
+export const textMdSemiBold = css`
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 24px;
+  font-style: normal;
+  vertical-align: middle;
+`;
