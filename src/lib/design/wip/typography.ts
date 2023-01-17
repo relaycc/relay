a87da87b0de3885fb25dbf5f7303b1227ed35253
achilles schmelzer <achilles@daopanel.com>
2023-01-17 00:45:45 -0800

    Add typography

--- src/lib/design/wip/typography.ts
+++ src/lib/design/wip/typography.ts
@@ -1,5 +1,55 @@
 import { css } from "styled-components";
 
+export const spaceMonoMdBold = css`
+  font-family: "Space Mono", monospace;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 24px;
+  font-style: normal;
+  vertical-align: middle;
+`;
+
+export const spaceMonoXsRegular = css`
+  font-family: "Space Mono", monospace;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 12px;
+  font-style: normal;
+  vertical-align: middle;
+`;
+
+export const displayXsBold = css`
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 32px;
+  font-style: normal;
+  vertical-align: middle;
+`;
+
+export const textXlSemibold = css`
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 30px;
+  font-style: normal;
+  vertical-align: middle;
+`;
+
+export const textMdSemiBold = css`
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 24px;
+  font-style: normal;
+  vertical-align: middle;
+`;
+
+export const textMdRegular = css`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+  font-style: normal;
+  vertical-align: middle;
+`;
+
 export const textSmallBold = css`
   font-size: 14px;
   line-height: 20px;
@@ -21,10 +71,10 @@ export const textXsMedium = css`
   font-style: normal;
 `;
 
-export const textMdSemiBold = css`
-  font-size: 16px;
-  font-weight: 600;
-  line-height: 24px;
+export const textXsRegular = css`
+  font-size: 12px;
+  line-height: 18px;
+  font-weight: 400;
   font-style: normal;
   vertical-align: middle;
 `;
