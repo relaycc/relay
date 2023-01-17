eb99c55835dfe3641cf047834ddd7a49903e5bbf
brian schafer <roycepdev@gmail.com>
2023-01-16 21:30:34 -0800

    Nav.tsx has been implemented and accepts 3 components and 3 functions

---
+++ src/lib/design/Nav.tsx
@@ -0,0 +1,60 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+
+interface NavProps {
+  LeftIcon: ReactNode;
+  CenterIcon: ReactNode;
+  RightIcon: ReactNode;
+  handleLeftIconClick: () => unknown;
+  handleCenterIconClick: () => unknown;
+  handleRightIconClick: () => unknown;
+}
+
+const Navbar = styled.nav`
+  background-color: white;
+  border-radius: 0px 0px 14px 14px;
+  width: 100%;
+  height: 80px;
+  border-top: 0.5px solid #D0D5DD;
+`;
+
+const NavItems = styled.ul`
+  list-style-type: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+  margin-block-start: 0em;
+  margin-block-end: 0em;
+  padding-inline-start: 0px;
+  border-radius: 0px 0px 14px 14px;
+  padding-left: 46px;
+  padding-right: 46px;
+`;
+
+const NavItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+   :first-of-type {
+    border-bottom-left-radius: 14px;
+   }
+
+   :last-of-type {
+    border-bottom-right-radius: 14px;
+   }
+`;
+
+export const Nav = ({ LeftIcon, CenterIcon, RightIcon, handleLeftIconClick, handleCenterIconClick, handleRightIconClick }: NavProps) => {
+  return (
+    <Navbar>
+      <NavItems>
+        <NavItem onClick={handleLeftIconClick}>{LeftIcon}</NavItem>
+        <NavItem onClick={handleCenterIconClick}>{CenterIcon}</NavItem>
+        <NavItem onClick={handleRightIconClick}>{RightIcon}</NavItem>
+      </NavItems>
+    </Navbar>
+  );
+};
