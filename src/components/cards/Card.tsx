ecdc6689afcad3d10aa5ec81e49efb26cc95c596
Sean Brennan <sean@relay.cc>
2023-01-18 18:58:36 -0500

    border on hover

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -9,7 +9,7 @@ export const Card = ({
 }) => {
   return (
     <div
-      className={`relative group w-[18rem] h-[24rem] flex flex-col shadow-lg rounded-xl gap-4 p-4 border-[3px] border-[#DAD8F6] bg-white ${className}`}
+      className={`relative group w-[18rem] h-[24rem] flex flex-col shadow-lg rounded-xl gap-4 p-4 border-[3px] border-[#DAD8F6] hover:border-[#4236C7] bg-white ${className}`}
     >
       {children}
     </div>
