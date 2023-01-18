0c36b78ae7bb91e028e04cd94dc55a7aa45335f0
PetMV <122448759+PetMV@users.noreply.github.com>
2023-01-18 18:29:52 +0100

    Closes #162
    Closes #163

---
+++ src/lib/design/AlertCircle.tsx
@@ -0,0 +1,10 @@
+export const AlertCircle = () => {
+  return (
+    <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="44" height="44" rx="22" fill="#FEF3F2" />
+      <path
+        d="M22 18V22M22 26H22.01M32 22C32 27.5228 27.5228 32 22 32C16.4772 32 12 27.5228 12 22C12 16.4772 16.4772 12 22 12C27.5228 12 32 16.4772 32 22Z"
+        stroke="#D92D20" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  );
+};
--- src/lib/design/XmtpIcon.tsx
+++ src/lib/design/XmtpIcon.tsx
@@ -1,4 +1,4 @@
-export const XmptIcon = () => {
+export const XmtpIcon = () => {
   return (
     <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect width="40" height="40" rx="20" fill="#857EEA"/>
