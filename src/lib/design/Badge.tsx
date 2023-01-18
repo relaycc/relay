fd3e6635f555abce345742166cdfad2d07219dcf
csillagk <katalin.csillag@emergence-engineering.com>
2023-01-18 16:59:44 +0100

    ENS ID and its building components

---
+++ src/lib/design/Badge.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import {receiverTheme} from "@/lib/design/wip/receiverTheme";
+import { textXsMedium} from "@/lib/design/wip/typography";
+
+
+const Root = styled.div`
+  display: flex;
+  
+  width: 100%;
+`;
+
+const Container = styled.div`
+  ${textXsMedium};
+  color: ${receiverTheme.colors.gray["700"]};
+  border-radius: 16px;
+  background-color: ${receiverTheme.colors.gray["100"]};
+  padding: 2px 8px;
+  height: 1.125rem;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoadingDiv = styled.div`
+  background: linear-gradient(90deg, #F1EFEF -24.18%, #F9F8F8 50.26%, #E7E5E5 114.84%);
+  border-radius: 16px;
+  height: 1.375rem;
+  
+  width: 5.75rem;
+`;
+
+export const Badge = ({isLoading}: {isLoading: boolean}) => {
+
+    if (isLoading){
+        return (
+            <Root>
+                <LoadingDiv />
+            </Root>
+        )
+    }
+    return(
+        <Root>
+            <Container>ETH Network</Container>
+        </Root>
+    )
+}
---
+++ src/lib/design/Time.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import {receiverTheme} from "@/lib/design/wip/receiverTheme";
+import {textXsRegular} from "@/lib/design/wip/typography";
+
+
+const Root = styled.div`
+  display: flex;
+  
+  width: 100%;
+`;
+
+const Container = styled.div`
+  ${textXsRegular};
+  color: ${receiverTheme.colors.gray["400"]};
+  letter-spacing: 0.03em;
+
+  height: 0.75rem;
+`;
+
+const LoadingDiv = styled.div`
+  background: linear-gradient(90deg, #F1EFEF -24.18%, #F9F8F8 50.26%, #E7E5E5 114.84%);
+  border-radius: 6px;
+  height: 0.75rem;
+  
+  width: 100%;
+`;
+
+export const Time = ({isLoading, time}: {isLoading: boolean, time: string}) => {
+
+    if (isLoading){
+        return (
+            <Root>
+                <LoadingDiv />
+            </Root>
+        )
+    }
+    return(
+        <Root>
+            <Container>{time}</Container>
+        </Root>
+    )
+}
