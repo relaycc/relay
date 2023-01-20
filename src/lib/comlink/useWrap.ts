f34796fc7c18d2dbc8e38e1774f00b596a6b6f45
achilles schmelzer <100799178+killthebuddh4@users.noreply.github.com>
2023-01-20 09:29:05 -0800

    Add hooks for comlink

---
+++ src/lib/comlink/useWrap.ts
@@ -0,0 +1,30 @@
+import * as Comlink from "comlink";
+import { useMemo, useEffect, useState } from "react";
+
+const COMLINK: {
+  wrapped: any;
+} = {
+  wrapped: null,
+};
+
+export const useWrap = (targ: unknown) => {
+  const [wrapped, setWrapped] = useState(false);
+
+  useEffect(() => {
+    if (COMLINK.wrapped === null) {
+      console.log(targ);
+      COMLINK.wrapped = Comlink.wrap(
+        Comlink.windowEndpoint((targ as any).current.contentWindow as Window)
+      );
+    }
+    setWrapped(true);
+  }, []);
+
+  return useMemo(() => {
+    if (wrapped && COMLINK.wrapped === null) {
+      throw new Error("Wrapped is true but COMLINK.wrapped is null");
+    } else {
+      return COMLINK.wrapped;
+    }
+  }, [wrapped]);
+};
