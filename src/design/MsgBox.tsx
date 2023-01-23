496b9dbecf16439e964ab5b3644711d5d3b67b97
csillagk <katalin.csillag@emergence-engineering.com>
2023-01-23 16:20:32 +0100

    MsgBox and NewMsgInbox got states and events

--- src/design/MsgBox.tsx
+++ src/design/MsgBox.tsx
@@ -1,16 +1,13 @@
 import styled from "styled-components";
-import { ArrowUpCircle } from "@/design/ArrowUpCircle";
-import { receiverTheme } from "@/design/receiverTheme";
-import { textSmallRegular } from "@/design/typography";
+import {ArrowUpCircle} from "@/design/ArrowUpCircle";
+import {receiverTheme} from "@/design/receiverTheme";
+import {textSmallRegular} from "@/design/typography";
+import {useState} from "react";
+import {LoaderAnimGeneral} from "@/design/LoaderAnimGeneral";
+import {IconContainer} from "@/design/NewMsgInput";
+
 
 const Root = styled.div`
-  display: flex;
-
-  width: 100%;
-  //width: 360px;
-`;
-
-const Wrapper = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
@@ -35,26 +32,46 @@ const Input = styled.input`
     ${textSmallRegular};
     color: ${receiverTheme.colors.gray["400"]};
   }
+
   width: 94%;
 `;
 
-export const MsgBox = ({ active }: { active: boolean }) => {
-  if (active) {
+const SendIconWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const MsgBox = ({handleSendIcon}: { handleSendIcon: () => unknown }) => {
+
+    const [message, setMessage] = useState("");
+    const [iconActive, setIconActive] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+// setLoading(true) is used when message is being sent
+
+    if (loading) {
+        return (
+            <Root>
+                <Input defaultValue={message}/>
+                <IconContainer>
+                    <LoaderAnimGeneral/>
+                </IconContainer>
+            </Root>
+        );
+    }
     return (
-      <Root>
-        <Wrapper>
-          <Input placeholder={"Type a Message"} />
-          <ArrowUpCircle active={active} />
-        </Wrapper>
-      </Root>
+        <Root>
+            <Input defaultValue={""} placeholder={"Type a Message"}
+                   onChange={() => {
+                       (e: any) => setMessage(e.target.value);
+                       setIconActive(true);
+                   }}/>
+            <SendIconWrapper onClick={() => handleSendIcon}>
+                <ArrowUpCircle active={iconActive}/>
+            </SendIconWrapper>
+        </Root>
     );
-  }
-  return (
-    <Root>
-      <Wrapper>
-        <Input placeholder={"Type a Message"} />
-        <ArrowUpCircle active={active} />
-      </Wrapper>
-    </Root>
-  );
 };
+
--- src/design/NewMsgInput.tsx
+++ src/design/NewMsgInput.tsx
@@ -1,7 +1,8 @@
-import styled, { css } from "styled-components";
-import { receiverTheme } from "@/design/receiverTheme";
-import { LoaderAnimGeneral } from "@/design/LoaderAnimGeneral";
-import { textMdRegular } from "@/design/typography";
+import styled, {css} from "styled-components";
+import {receiverTheme} from "@/design/receiverTheme";
+import {LoaderAnimGeneral} from "@/design/LoaderAnimGeneral";
+import {textMdRegular} from "@/design/typography";
+import {useState} from "react";
 
 const Root = styled.div`
   height: 2.5rem;
@@ -22,28 +23,34 @@ const To = styled.div`
   width: 6%;
 `;
 
-const TextInput = styled.input<{ isLoading: boolean }>`
+export const TextInput = styled.input<{ loading: boolean }>`
   ${textMdRegular};
   border: hidden;
   color: ${receiverTheme.colors.gray["900"]};
 
-  ${({ isLoading }) =>
-    isLoading &&
-    css`
-      color: ${receiverTheme.colors.gray["400"]};
-    `};
+  ${({loading}) =>
+          loading &&
+          css`
+            color: ${receiverTheme.colors.gray["400"]};
+          `};
 
   :focus {
     outline: none;
   }
 
+  ::placeholder {
+    color: ${receiverTheme.colors.gray["300"]};
+    caret-color: auto;
+  }
+
   width: 75%;
 `;
 
-const IconContainer = styled.div`
+export const IconContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  cursor: pointer;
 
   width: 1.25rem;
   height: 1.25rem;
@@ -58,40 +65,50 @@ const AddIcon = styled.div<{ active: boolean }>`
   font-size: 1.5rem;
   color: ${receiverTheme.colors.gray["400"]};
 
-  ${({ active }) =>
-    active &&
-    css`
-      color: ${receiverTheme.colors.gray["900"]};
-    `};
+  ${({active}) =>
+          active &&
+          css`
+            color: ${receiverTheme.colors.gray["900"]};
+          `};
 `;
 
 export const NewMsgInput = ({
-  active,
-  isLoading,
-  value,
-}: {
-  active: boolean;
-  isLoading: boolean;
-  value: string;
+                                handleAddingAddress,
+                                placeholder
+                            }: {
+    placeholder: string;
+    handleAddingAddress: () => unknown;
 }) => {
-  if (isLoading) {
+
+    const [to, setTo] = useState("");
+    const [iconActive, setIconActive] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    // setIconActive(true) is used when the ENS/Lens/address is found (or chosen from list?)
+
+    if (loading) {
+        return (
+            <Root>
+                <To>To: </To>
+                <TextInput defaultValue={to} loading={loading}/>
+                <IconContainer>
+                    <LoaderAnimGeneral/>
+                </IconContainer>
+            </Root>
+        );
+    }
+
     return (
-      <Root>
-        <To>To: </To>
-        <TextInput value={value} isLoading={isLoading} />
-        <IconContainer>
-          <LoaderAnimGeneral />
-        </IconContainer>
-      </Root>
+        <Root>
+            <To>To: </To>
+            <TextInput defaultValue={to} onChange={() => {
+                (e: any) => setTo(e.target.value);
+                setLoading(true);
+            }
+            } loading={loading} placeholder={placeholder}/>
+            <IconContainer>
+                <AddIcon active={iconActive} onClick={handleAddingAddress}>+</AddIcon>
+            </IconContainer>
+        </Root>
     );
-  }
-  return (
-    <Root>
-      <To>To: </To>
-      <TextInput value={value} isLoading={isLoading} />
-      <IconContainer>
-        <AddIcon active={active}>+</AddIcon>
-      </IconContainer>
-    </Root>
-  );
-};
+}
