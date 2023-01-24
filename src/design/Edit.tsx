2dd09711419d460832def98552f3a7339dceb0cf
PetMV <122448759+PetMV@users.noreply.github.com>
2023-01-24 15:23:10 +0100

    fixed Edit

--- src/design/Edit.tsx
+++ src/design/Edit.tsx
@@ -1,7 +1,25 @@
 import styled, { css } from "styled-components";
+import { ComponentProps } from "react";
 
-const Svg = styled.svg<{isActive: boolean}>`
-    ${({isActive}) => isActive ? css`
+
+export const Edit = styled((props: ComponentProps<"svg"> & {isActive: boolean}) =>(
+  <svg
+    width="40"
+    height="40"
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <rect width="40" height="40" rx="20" fill="white"/>
+    <path d="M20 28H29M24.5 11.5C24.8978 11.1022 25.4374 10.8787 26 10.8787C26.2786 10.8787 26.5544 10.9335 26.8118 11.0402C27.0692 11.1468 27.303 11.303 27.5 11.5C27.697 11.697 27.8532 11.9308 27.9598 12.1882C28.0665 12.4456 28.1213 12.7214 28.1213 13C28.1213 13.2786 28.0665 13.5544 27.9598 13.8118C27.8532 14.0692 27.697 14.303 27.5 14.5L15 27L11 28L12 24L24.5 11.5Z"
+          stroke="#101828"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"/>
+</svg>
+    ))`
+  ${({isActive}) => isActive ? css`
         rect {
           transition: fill 150ms ease-in;
           fill: #4236C7;
@@ -23,14 +41,4 @@ const Svg = styled.svg<{isActive: boolean}>`
                 fill: #D0D5DD;
             }
         }
-    `}
-`
-
-export const Edit = ({isActive}: {isActive: boolean}) => {
-    return (
-        <Svg isActive={isActive} width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="40" height="40" rx="20" fill="white"/>
-    <path d="M20 28H29M24.5 11.5C24.8978 11.1022 25.4374 10.8787 26 10.8787C26.2786 10.8787 26.5544 10.9335 26.8118 11.0402C27.0692 11.1468 27.303 11.303 27.5 11.5C27.697 11.697 27.8532 11.9308 27.9598 12.1882C28.0665 12.4456 28.1213 12.7214 28.1213 13C28.1213 13.2786 28.0665 13.5544 27.9598 13.8118C27.8532 14.0692 27.697 14.303 27.5 14.5L15 27L11 28L12 24L24.5 11.5Z" stroke="#101828" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-</Svg>
-    )
-}
+    `}`;
