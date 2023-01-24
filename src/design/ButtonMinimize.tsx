93af53766544bd57752b85c32dad4050946cf194
PetMV <122448759+PetMV@users.noreply.github.com>
2023-01-24 14:31:09 +0100

    fixed ButtonMinimize

--- src/design/ButtonMinimize.tsx
+++ src/design/ButtonMinimize.tsx
@@ -1,7 +1,28 @@
 import styled from "styled-components";
 import { receiverTheme } from "@/design/receiverTheme";
+import { ComponentProps } from "react";
 
-const Svg = styled.svg`
+
+
+export const ButtonMinimize = styled((props: ComponentProps<"svg">) => (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <rect width="40" height="40" rx="20" fill="white" />
+      <path
+        d="M13 20H27"
+        stroke="#101828"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ))`
   :hover {
     rect {
       fill: ${receiverTheme.colors.gray["200"]};
@@ -12,26 +33,4 @@ const Svg = styled.svg`
     rect {
       fill: ${receiverTheme.colors.gray["300"]};
     }
-  }
-`;
-
-export const ButtonMinimize = () => {
-  return (
-    <Svg
-      width="40"
-      height="40"
-      viewBox="0 0 40 40"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect width="40" height="40" rx="20" fill="white" />
-      <path
-        d="M13 20H27"
-        stroke="#101828"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </Svg>
-  );
-};
+  }`;
