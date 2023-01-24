4daa76ab2df144a56d1b4773b39549c1c2a7582e
PetMV <122448759+PetMV@users.noreply.github.com>
2023-01-24 14:24:48 +0100

    fixed BackIcon

--- src/design/BackIcon.tsx
+++ src/design/BackIcon.tsx
@@ -1,6 +1,25 @@
 import styled from "styled-components";
+import { ComponentProps } from "react";
 
-const Svg = styled.svg`
+
+export const BackIcon = styled((props: ComponentProps<"svg">) => (
+
+    <svg
+      width="44"
+      height="44"
+      viewBox="0 0 44 44"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <rect width="44" height="44" rx="22" fill="white"/>
+      <path
+        d="M29 22H15M15 22L22 29M15 22L22 15"
+        stroke="#101828" strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"/>
+    </svg>
+    ))`
   :hover {
     rect {
       transition: fill 150ms ease-in;
@@ -9,17 +28,7 @@ const Svg = styled.svg`
   }
   :active {
     rect {
-       transition: fill 150ms ease-in;
-       fill: #D0D5DD;
-     }
-   }
-`
-
-export const BackIcon = () => {
-  return (
-    <Svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="44" height="44" rx="22" fill="white"/>
-      <path d="M29 22H15M15 22L22 29M15 22L22 15" stroke="#101828" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    </Svg>
-    )
-}
+      transition: fill 150ms ease-in;
+      fill: #D0D5DD;
+    }
+  }`;
