5d0e235624f05b853b886fe5d17fa50ea9945c1f
PetMV <122448759+PetMV@users.noreply.github.com>
2023-01-24 15:06:58 +0100

    fixed DropdownIcon

--- src/design/DropdownIcon.tsx
+++ src/design/DropdownIcon.tsx
@@ -1,20 +1,25 @@
 import styled, { css } from "styled-components";
+import { ComponentProps } from "react";
 
-const Svg = styled.svg<{isOpen: boolean}>`
+
+
+export const DropdownIcon = styled((props: ComponentProps<"svg"> & {isOpen: boolean}) => (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M9 18L15 12L9 6" stroke="#101828" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+
+  ))`
   ${({isOpen}) => isOpen && css`
     path {
       transition: transform 150ms ease-in;
       transform: rotate(90deg);
       transform-origin: 50% 50%;
     }
-`}
-`
-
-export const DropdownIcon = ({isOpen}: {isOpen: boolean}) => {
-  return (
-    <Svg isOpen={isOpen} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M9 18L15 12L9 6" stroke="#101828" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    </Svg>
-
-  )
-}
+`};`
