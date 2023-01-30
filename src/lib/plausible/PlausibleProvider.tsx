5cf1a6e8b7bd46332335970ed97173679d4c0803
achilles schmelzer <achilles@daopanel.com>
2023-01-30 09:02:13 -0800

    Vercel deploy test

--- src/lib/plausible/PlausibleProvider.tsx
+++ src/lib/plausible/PlausibleProvider.tsx
@@ -5,6 +5,8 @@ import { IS_PRODUCTION_DEPLOYMENT, DOMAIN } from "@/lib/vercel";
 export const PlausibleProvider: FunctionComponent<{ children: ReactNode }> = ({
   children,
 }) => {
+  console.log("ENV", process.env.NEXT_PUBLIC_VERCEL_ENV);
+  console.log("URL", process.env.NEXT_PUBLIC_VERCEL_URL);
   return (
     <BasePlausibleProvider
       domain={DOMAIN}
