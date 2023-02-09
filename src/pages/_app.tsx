ba9fcfc4ac367a339a66469c8a77693121a32995
Istvan Matejcsok <119620946+matejcsok-ee@users.noreply.github.com>
2023-02-09 17:36:45 +0100

    [fix] FOUT

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,8 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { XmtpProvider } from "@relaycc/xmtp-hooks";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { PlausibleProvider } from "@/lib/plausible/PlausibleProvider";
+import "../styles/baseStyles.css";
+import Head from "next/head";
 
 export const { chains, provider } = configureChains(
   [mainnet, polygon, optimism, arbitrum],
@@ -33,25 +35,57 @@ export default function App({ Component, pageProps }: AppProps) {
     setWorker(new Worker("/xmtp.js"));
   }, []);
   return (
-    <PlausibleProvider>
-      <ReceiverThemeProvider>
-        <WagmiConfig client={client}>
-          <RainbowKitProvider chains={chains}>
-            <GlobalStyles />
-            {(() => {
-              if (worker === null) {
-                return null;
-              } else {
-                return (
-                  <XmtpProvider config={{ worker: worker as Worker }}>
-                    <Component {...pageProps} />
-                  </XmtpProvider>
-                );
-              }
-            })()}
-          </RainbowKitProvider>
-        </WagmiConfig>
-      </ReceiverThemeProvider>
-    </PlausibleProvider>
+    <>
+      <Head>
+        <link
+          rel="preload"
+          as="font"
+          href="/fonts/Satoshi-Regular.woff2"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          as="font"
+          href="/fonts/Satoshi-Medium.woff2"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          as="font"
+          href="/fonts/Satoshi-Bold.woff2"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          as="font"
+          href="/fonts/Satoshi-Black.woff2"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <PlausibleProvider>
+        <ReceiverThemeProvider>
+          <WagmiConfig client={client}>
+            <RainbowKitProvider chains={chains}>
+              <GlobalStyles />
+              {(() => {
+                if (worker === null) {
+                  return null;
+                } else {
+                  return (
+                    <XmtpProvider config={{ worker: worker as Worker }}>
+                      <Component {...pageProps} />
+                    </XmtpProvider>
+                  );
+                }
+              })()}
+            </RainbowKitProvider>
+          </WagmiConfig>
+        </ReceiverThemeProvider>
+      </PlausibleProvider>
+    </>
   );
 }
